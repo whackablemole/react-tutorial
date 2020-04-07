9a9540842c43425ec488bf2e1564dcb98e48f5a6
Mark Townsend <mark.townsend@semcon.com>
2020-04-07 12:44:35 +0100

    Creating image element and dynamically applying a random image URL.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,13 +3,15 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    imageUrl: "https://picsum.photos/200",
   };
   render() {
     return (
-      <React.Fragment>
+      <div>
+        <img src={this.state.imageUrl} alt="" />
         <span>{this.formatCount()}</span>
         <button>Increment</button>
-      </React.Fragment>
+      </div>
     ); //<button>Increment</button>
   }
 
