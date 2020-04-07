335a0ce2af5a762710e32e9a24e9a1ef76bbb8f8
Mark Townsend <mark.townsend@semcon.com>
2020-04-07 11:22:41 +0100

    Hello world section complete.

---
+++ react-app/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+// eslint-disable-next-line
+import ReactDOM from "react-dom";
+
+const element = <h1>Hello World</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
