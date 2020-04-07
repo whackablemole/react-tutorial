c139bc440f49f653d894e2e23a70dad9c207b411
Mark Townsend <mark.townsend@semcon.com>
2020-04-07 14:35:28 +0100

    Deleted all children.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -16,10 +16,8 @@ class Counter extends Component {
   };
 
   render() {
-    console.log("props", this.props);
     return (
       <div>
-        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement({ id: 1 })}
