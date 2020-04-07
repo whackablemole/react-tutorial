1fb6d9a807bc4e1c6adef54f0a7199e7fd4c3673
Mark Townsend <mark.townsend@semcon.com>
2020-04-07 12:49:00 +0100

    Using styles in a style property.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,14 +3,20 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200",
   };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
+
   render() {
     return (
       <div>
-        <img src={this.state.imageUrl} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     ); //<button>Increment</button>
   }
