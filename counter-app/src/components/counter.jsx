b9270566c3d2e89ab562fd9e6f3eff2f55e06d22
Mark Townsend <mark.townsend@semcon.com>
2020-04-07 14:03:03 +0100

    Example of passing values to child components.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
 
   //   constructor() {
@@ -12,10 +12,11 @@ class Counter extends Component {
 
   // Arrow functions automatically inherit "this" from the component
   handleIncrement = (product) => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
+    console.log("props", this.props);
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -31,12 +32,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -4,17 +4,18 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 0 },
-      { id: 1, value: 0 },
-      { id: 1, value: 0 },
-      { id: 1, value: 0 },
+      { id: 1, value: 4 },
+      { id: 2, value: 69 },
+      { id: 3, value: 2 },
+      { id: 4, value: 0 },
     ],
   };
+
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} />
+          <Counter key={counter.id} value={counter.value} />
         ))}
       </div>
     );
