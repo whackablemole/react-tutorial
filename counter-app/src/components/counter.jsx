3d32eafc0fae9df18be4910d309c7bd59b63baf9
Mark Townsend <mark.townsend@semcon.com>
2020-04-07 12:57:58 +0100

    Example of assigning a className dependent on property values.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -8,12 +8,16 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     ); //<button>Increment</button>
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
